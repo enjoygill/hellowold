4ad448a5e2294397072270cc79cef1a6cd718f9f
enjoygill <1873247496@qq.com>
2018-03-15 17:06:04 +0800

    java项目

---
+++ src/helloworld/helloworld.java
@@ -0,0 +1,8 @@
+package helloworld;
+public class helloworld {        
+             public static void main(String[] args) {
+				      int a=1;
+				      int b=2;
+				      System.out.println(++a/2);			    		
+			}
+}
