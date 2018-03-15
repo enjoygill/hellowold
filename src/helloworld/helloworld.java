77f3c814c3fadc66f53af8369b01f1efc3d4af0f
enjoygill <1873247496@qq.com>
2018-03-15 17:23:30 +0800

    修改c

--- src/helloworld/helloworld.java
+++ src/helloworld/helloworld.java
@@ -3,6 +3,7 @@ public class helloworld {
              public static void main(String[] args) {
 				      int a=1;
 				      int b=2;
+				      int c=3;
 				      System.out.println(++a/2);			    		
 			}
 }
